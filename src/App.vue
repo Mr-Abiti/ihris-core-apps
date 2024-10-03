ac77e1f58df5728509f21542a1d9aee3f79db523
Muluken Bogale <mulukenb89@gmail.com>
2024-10-03 13:40:48 -0400

    modify header text format

--- src/App.vue
+++ src/App.vue
@@ -27,10 +27,7 @@
       >
         {{ connectionStatus }}
       </v-alert>
-      <center>
-        <p></p>
-        <b>iHRIS Translation App</b>
-      </center>
+      <h3 class="text-center mt-4">iHRIS Translation App</h3>
       <router-view :key="$route.fullPath" />
     </v-main>
     <IhrisFooter :footer="footer" />
